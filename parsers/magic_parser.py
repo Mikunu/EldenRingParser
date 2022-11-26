ebf851a428112178da794c8dff4aedeb40322858
Mikunu <nonamebeautifultrap@gmail.com>
2022-11-27 02:03:36 +0600

    Fixed weapon and magic parsers. Disabled 'Duration' in magic

--- parsers/magic_parser.py
+++ parsers/magic_parser.py
@@ -5,67 +5,77 @@ from bs4 import BeautifulSoup
 
 
 def get_magic_stats_from_dict(stats: dict) -> str:
-    if stats['Magic School Type'] == 0:
+    if stats['ezStateBehaviorType'] == 0:
         magic_school_type = 'Магия'
     else:
         magic_school_type = 'Молитва'
     stat_s = f'Общее:\n' \
              f'  Вид заклинания: {magic_school_type}\n' \
-             f'  Расход ОК: {stats["FP Cost"]}\n' \
-             f'  Расход выносливости: : {stats["Stamina Cost"]}\n' \
-             f'  Ячейки: {stats["Slot Usage"]}\n' \
-             f'  Длительность: {stats["duration"]}\n' \
-             f'  Ловкость скорость каста минимум: {stats["DEX Casting Speed Min"]}\n' \
-             f'  Ловкость скорость каста максимум: {stats["DEX Casting Speed Max"]}\n' \
+             f'  Расход ОК: {stats["mp"]}\n' \
+             f'  Расход выносливости: : {stats["stamina"]}\n' \
+             f'  Ячейки: {stats["slotLength"]}\n' \
+             f'  Длительность: {stats["effectEndurance"]}\n' \
              f'Требования:\n' \
-             f'  Муд: {stats["Requirement: INT"]}\n' \
-             f'  Вер: {stats["Requirement: FTH"]}\n' \
-             f'  Кол: {stats["Requirement: ARC"]}'
+             f'  Муд: {stats["requirementIntellect"]}\n' \
+             f'  Вер: {stats["requirementFaith"]}\n' \
+             f'  Кол: {stats["requirementLuck"]}'
     return stat_s
 
 
 class MagicParser:
     def __init__(self, path: str):
         self.base_folder_path = path
-        self.df_magic: pd.DataFrame = pd.read_csv(os.path.join(path, 'Magic.csv'),
-                                                  delimiter=';', on_bad_lines='skip', low_memory=False)
-        self.df_duration: pd.DataFrame = pd.read_csv(os.path.join(path, 'SpEffectParam.csv'),
-                                                     delimiter=';', on_bad_lines='skip', low_memory=False)
+        self.df_magic: pd.DataFrame = pd.read_csv(os.path.join(path, 'stats/Magic.csv'),
+                                                  delimiter=',', on_bad_lines='skip', low_memory=False)
+        self.df_duration: pd.DataFrame = pd.read_csv(os.path.join(path, 'stats/SpEffectParam.csv'),
+                                                     delimiter=',', on_bad_lines='skip', low_memory=False)
 
     def get_stats_magic(self, item_id: int):
-        magic = self.df_magic.loc[self.df_magic['Row ID'] == item_id]
-        eng_name = magic.iloc[0]['Row Name']
-        duration = \
-            self.df_duration.loc[self.df_duration['Row Name'] == eng_name][['Duration']].to_dict('list')['Duration']
-        stats = magic[['Requirement: ARC',
-                       'Requirement: INT',
-                       'Requirement: FTH',
-                       'FP Cost',
-                       'Stamina Cost',
-                       'Slot Usage',
-                       'DEX Casting Speed Min',
-                       'DEX Casting Speed Max',
-                       'Magic School Type']]
+        magic = self.df_magic.loc[self.df_magic['ID'] == item_id]
+        eng_name = magic.iloc[0]['Name']
+
+        # region Duration
+        # todo Fix Duration
+        '''
+        duration_id = magic[['refId1']]
+
+        try:
+            duration = self.df_duration.loc[self.df_duration['ID'] == duration_id][
+                ['effectEndurance']].to_dict('list')['effectEndurance'][0]
+        except KeyError as err_name:
+            print(f'{err_name} with {item_id} ({eng_name})')
+            duration = '-'
+        '''
+        # endregion
+        stats = magic[['requirementLuck',
+                       'requirementIntellect',
+                       'requirementFaith',
+                       'mp',
+                       'stamina',
+                       'slotLength',
+                       'analogDexterityMin',
+                       'analogDexterityMax',
+                       'ezStateBehaviorType']]
         stats = stats.to_dict('list')
-        stats['duration'] = duration
+        stats['effectEndurance'] = '-'
         for key, value in stats.items():
             stats[key] = value[0]
         return stats
 
     def write_magic_to_file(self, path_to_write: str = ''):
-        with open(os.path.join(self.base_folder_path, 'GoodsNameEng.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, 'en/GoodsName.fmg.xml'), 'rb') as f:
             eng_names = BeautifulSoup(f, 'lxml')
 
-        with open(os.path.join(self.base_folder_path, 'GoodsName.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, 'ru/GoodsName.fmg.xml'), 'rb') as f:
             names = BeautifulSoup(f, 'lxml')
 
-        with open(os.path.join(self.base_folder_path, 'GoodsInfo.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, 'ru/GoodsInfo.fmg.xml'), 'rb') as f:
             captions = BeautifulSoup(f, 'lxml')
         # I don't know why I wrote this and why it's useless
-        with open(os.path.join(self.base_folder_path, 'GoodsInfo2.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, 'ru/GoodsInfo2.fmg.xml'), 'rb') as f:
             captions2 = BeautifulSoup(f, 'lxml')
 
-        file = open(os.path.join(path_to_write, 'magic.txt'), 'a', encoding='utf-8')
+        file = open(os.path.join(path_to_write, 'magic.txt'), 'w', encoding='utf-8')
 
         items_ids = get_items_ids(self.df_magic)
 
--- parsers/weapon_parser.py
+++ parsers/weapon_parser.py
@@ -6,33 +6,33 @@ from bs4 import BeautifulSoup
 
 def get_weapon_stats_from_dict(weapon_stats: dict) -> str:
     return f'Атака: ' \
-           f'Физическ.: {weapon_stats["Damage: Physical"]} ' \
-           f'Магия: {weapon_stats["Damage: Magic"]}, ' \
-           f'Огонь: {weapon_stats["Damage: Fire"]}, ' \
-           f'Молния: {weapon_stats["Damage: Lightning"]}, ' \
-           f'Святое: {weapon_stats["Damage: Holy"]}, ' \
-           f'Крит. удар: {weapon_stats["Critical"] + 100}\n' \
+           f'Физическ.: {weapon_stats["attackBasePhysics"]} ' \
+           f'Магия: {weapon_stats["attackBaseMagic"]}, ' \
+           f'Огонь: {weapon_stats["attackBaseFire"]}, ' \
+           f'Молния: {weapon_stats["attackBaseThunder"]}, ' \
+           f'Святое: {weapon_stats["attackBaseDark"]}, ' \
+           f'Крит. удар: {weapon_stats["throwAtkRate"] + 100}\n' \
            f'Защита :' \
-           f'Физическ.: {weapon_stats["Guard Absorption: Physical"]}, ' \
-           f'Магия: {weapon_stats["Guard Absorption: Magic"]}, ' \
-           f'Огонь: {weapon_stats["Guard Absorption: Fire"]}, ' \
-           f'Молния: {weapon_stats["Guard Absorption: Lightning"]}, ' \
-           f'Святое: {weapon_stats["Guard Absorption: Holy"]}, ' \
-           f'Усил. блок {weapon_stats["Guard Stability"]}\n' \
-           f'Вес: {weapon_stats["Weight"]}, ' \
+           f'Физическ.: {weapon_stats["physGuardCutRate"]}, ' \
+           f'Магия: {weapon_stats["magGuardCutRate"]}, ' \
+           f'Огонь: {weapon_stats["fireGuardCutRate"]}, ' \
+           f'Молния: {weapon_stats["thunGuardCutRate"]}, ' \
+           f'Святое: {weapon_stats["darkGuardCutRate"]}, ' \
+           f'Усил. блок {weapon_stats["staminaGuardDef"]}\n' \
+           f'Вес: {weapon_stats["weight"]}, ' \
            f'Цена: ' \
            f'Покупка: [NO INFO], ' \
-           f'Продажа: {weapon_stats["Sell Price"]}'
+           f'Продажа: {weapon_stats["sellValue"]}'
 
 
 class WeaponParser:
 
     def __init__(self, path: str):
         self.base_folder_path = path
-        self.df_weapon: pd.DataFrame = pd.read_csv(os.path.join(self.base_folder_path, 'EquipParamWeapon.csv'),
-                                                   delimiter=';', index_col=False)
-        self.df_weapon_art: pd.DataFrame = pd.read_csv(os.path.join(self.base_folder_path, 'SwordArtsParam.csv'),
-                                                   delimiter=';', index_col=False)
+        self.df_weapon: pd.DataFrame = pd.read_csv(os.path.join(self.base_folder_path, 'stats/EquipParamWeapon.csv'),
+                                                   delimiter=',', index_col=False)
+        self.df_weapon_art: pd.DataFrame = pd.read_csv(os.path.join(self.base_folder_path, 'stats/SwordArtsParam.csv'),
+                                                   delimiter=',', index_col=False)
 
     def get_weapon_stats(self, item_id: int) -> dict:
         """
@@ -41,12 +41,23 @@ class WeaponParser:
         :param item_id: weapon id
         :return: dict with weapon's stats
         """
-        weapon = self.df_weapon.loc[self.df_weapon['Row ID'] == item_id]
-        weapon_stats = weapon[['attackBasePhysics', 'attackBaseMagic', 'attackBaseFire', 'attackBaseThunder', 'attackBaseDark',
-                               'Critical',
-                               'Guard Absorption: Physical', 'Guard Absorption: Magic', 'Guard Absorption: Fire',
-                               'Guard Absorption: Lightning', 'Guard Absorption: Holy', 'Guard Stability',
-                               'Weight', 'Sell Price', 'Weapon Category']]
+        weapon = self.df_weapon.loc[self.df_weapon['ID'] == item_id]
+        weapon_stats = weapon[['attackBasePhysics',
+                               'attackBaseMagic',
+                               'attackBaseFire',
+                               'attackBaseThunder',
+                               'attackBaseDark',
+                               'attackBaseStamina',
+                               'throwAtkRate',
+                               'physGuardCutRate',
+                               'magGuardCutRate',
+                               'fireGuardCutRate',
+                               'thunGuardCutRate',
+                               'darkGuardCutRate',
+                               'staminaGuardDef',
+                               'weight',
+                               'sellValue',
+                               'weaponCategory']]
         weapon_stats = weapon_stats.to_dict('list')
         for key, value in weapon_stats.items():
             weapon_stats[key] = value[0]
@@ -54,11 +65,11 @@ class WeaponParser:
         return weapon_stats
 
     def get_weapon_art(self, item_id: int):
-        weapon_art_row = self.df_weapon.loc[self.df_weapon['Row ID'] == item_id]
+        weapon_art_row = self.df_weapon.loc[self.df_weapon['ID'] == item_id]
         return weapon_art_row['swordArtsParamId'].item()
 
     def get_required_stats(self, item_id: int):
-        weapon = self.df_weapon.loc[self.df_weapon['Row ID'] == item_id]
+        weapon = self.df_weapon.loc[self.df_weapon['ID'] == item_id]
         requirements = weapon[['properStrength', 'properAgility', 'properMagic',
                                'properFaith', 'properLuck']]
         requirements = requirements.to_dict('list')
@@ -70,24 +81,24 @@ class WeaponParser:
         return requirements
 
     def write_weapons_to_file(self, path_to_write: str = ''):
-        with open(os.path.join(self.base_folder_path, r'WeaponName.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, r'ru/WeaponName.fmg.xml'), 'rb') as f:
             names = BeautifulSoup(f, 'lxml')
 
-        with open(os.path.join(self.base_folder_path, r'WeaponNameEng.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, r'en/WeaponName.fmg.xml'), 'rb') as f:
             eng_names = BeautifulSoup(f, 'lxml')
 
-        with open(os.path.join(self.base_folder_path, r'WeaponCaption.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, r'ru/WeaponCaption.fmg.xml'), 'rb') as f:
             captions = BeautifulSoup(f, 'lxml')
 
-        with open(os.path.join(self.base_folder_path, r'ArtsName.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, r'ru/ArtsName.fmg.xml'), 'rb') as f:
             arts_names = BeautifulSoup(f, 'lxml')
 
-        with open(os.path.join(self.base_folder_path, r'ArtsCaption.fmg.xml'), 'rb') as f:
+        with open(os.path.join(self.base_folder_path, r'ru/ArtsCaption.fmg.xml'), 'rb') as f:
             arts_captions = BeautifulSoup(f, 'lxml')
 
         print('All weapon data loaded')
 
-        file = open(os.path.join(path_to_write, 'weapons.txt'), 'a', encoding='utf-8')
+        file = open(os.path.join(path_to_write, 'weapons.txt'), 'w', encoding='utf-8')
 
         items_ids = get_items_ids(self.df_weapon)
         prev_item_id = -1
@@ -104,9 +115,12 @@ class WeaponParser:
             caption = captions.find(id=item_id)
             weapon_art_id = self.get_weapon_art(item_id)
 
-            weapon_art = self.df_weapon_art.loc[self.df_weapon_art['Row ID'] == weapon_art_id]
-            weapon_art_fp_cons = weapon_art[['useMagicPoint_L1', 'useMagicPoint_L2',
+            weapon_art = self.df_weapon_art.loc[self.df_weapon_art['ID'] == weapon_art_id]
+            weapon_art_fp_cons_dict = weapon_art[['useMagicPoint_L1', 'useMagicPoint_L2',
                                              'useMagicPoint_R1', 'useMagicPoint_R2']].to_dict('list')
+            weapon_art_fp_cons = weapon_art_fp_cons_dict[
+                max(weapon_art_fp_cons_dict, key=weapon_art_fp_cons_dict.get)][0]
+
             if weapon_art_id == 0:
                 art_name = '<text></text>'
                 art_caption = '<text></text>'
@@ -115,15 +129,20 @@ class WeaponParser:
                 art_caption = arts_captions.find(id=weapon_art_id)
 
             if name is None or eng_name is None or caption is None or art_name is None or art_caption is None:
+                print(f'Problem with {item_id}')
                 continue
             prev_item_id = item_id
             try:
-                info_s = f'{name.text} (англ. {eng_name.text})\n{caption.text}{art_caption.text}\nТраты ОК: {weapon_art_fp_cons}'
+                info_s = f'{name.text} (англ. {eng_name.text})\n{caption.text}\n' \
+                         f'{art_caption.text}\n' \
+                         f'Траты ОК: {weapon_art_fp_cons}'
                 requirements_s = get_required_stats_from_dict(self.get_required_stats(item_id))
                 stats_s = get_weapon_stats_from_dict(self.get_weapon_stats(item_id))
                 result = f'{info_s}\n{requirements_s}\n{stats_s}\n{"-" * 50}\n'
                 result = result.replace('\n\n\n', '\n')
                 file.write(result)
-                print(f'"{name.text}" proceeded')
+                # print(f'"{name.text}" proceeded')
             except Exception as e:
                 print(f'Exception with item_id: {item_id}. {e}')
+
+        print('All weapon proceeded')
